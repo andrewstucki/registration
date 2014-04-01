f44833973bfb4d21ba16c169188e02d0a5113aa2
Andrew Stucki <andrew.stucki@gmail.com>
2014-04-01 15:30:26 -0500

    Add windows started work with custom dll wrappers

--- server/network/interface_windows.go
+++ server/network/interface_windows.go
@@ -1,14 +1,15 @@
 package network
 
-// #cgo LDFLAGS: -lwlanapi -lole32
+// #cgo CFLAGS: -I../../include
+// #cgo LDFLAGS: -lole32 -L../../lib -lwlanapi
 // #define UNICODE
+// #define  WLAN_NOTIFICATION_SOURCE_ALL 0X0000FFFF
 // #include <windows.h>
 // #include <wlanapi.h>
 // #include <objbase.h>
 // #include <wtypes.h>
 // #include <stdio.h>
 // #include <stdlib.h>
-//
 // char** enumerateInterfaces() {
 //   HANDLE hClient = NULL;
 //   DWORD dwMaxClient = 2;
@@ -30,7 +31,34 @@ package network
 //   return interfaces;
 // }
 //
+// void onNotification(PWLAN_NOTIFICATION_DATA data, PVOID context) {
+//   printf("%x, %x, %s", data->NotificationSource, data->NotificationCode, data->InterfaceGuid);
+// }
+//
 // char * setupAdhocNetwork(char * iface, char * ssid, unsigned int securityMode, unsigned int channel, char * password) {
+//   HANDLE hClient = NULL;
+//   DWORD dwMaxClient = 2;
+//   DWORD dwCurVersion = 0;
+//   PDWORD source = NULL;
+//   PDWORD rc = NULL;
+//   // WLAN_HOSTED_NETWORK_STATUS status = {0};
+//   ULONG ssidLength = strlen(ssid);
+//   int enabled = 1;
+//   DOT11_SSID ssidStruct;
+//   memset(&ssidStruct, 0, sizeof(ssidStruct));
+//   ssidStruct.uSSIDLength = ssidLength;
+//   memcpy(ssidStruct.ucSSID, ssid, ssidLength);
+//
+//   WLAN_HOSTED_NETWORK_CONNECTION_SETTINGS settings = { ssidStruct, 100 };
+//   WlanOpenHandle(dwMaxClient, NULL, &dwCurVersion, &hClient);
+//   WlanRegisterNotification(hClient, WLAN_NOTIFICATION_SOURCE_ALL, 1, &onNotification, NULL, NULL, source);
+//   WlanHostedNetworkInitSettings(hClient, rc, NULL);
+//   WlanHostedNetworkSetProperty(hClient, wlan_hosted_network_opcode_enable, sizeof(enabled), &enabled, rc, NULL);
+//   WlanHostedNetworkSetProperty(hClient, wlan_hosted_network_opcode_connection_settings, sizeof(settings), &settings, rc, NULL);
+//   WlanHostedNetworkSetSecondaryKey(hClient, strlen(password)+1, password, 1, 1, rc, NULL);
+//   WlanHostedNetworkStartUsing(hClient, rc, NULL);
+//   // WlanHostedNetworkQueryStatus(hClient, &status, NULL);
+//
 //   return "localhost";
 // }
 import "C"
